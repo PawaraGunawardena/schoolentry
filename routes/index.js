9798118764fef639f01a01ab9b3d393470813642
Dasun Pubudumal <dasun.15@cse.mrt.ac.lk>
2017-11-05 14:38:32 +0530

    "Docs added"

--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,7 @@ var path = require('path');
 /* GET home page. */
 router.get('/', function(req, res, next) {
   // res.render('index', { title: 'Express' });
-    res.sendFile(path.join(__dirname + '/../index.html'));
+    res.sendFile(path.join(__dirname + '/../pages/index.html'));
 });
 
 router.get('/register', function(req, res, next) {
