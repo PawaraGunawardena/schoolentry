b055356adf0fbc8e6097b86074666331fd17529a
Dasun Pubudumal <dasun.15@cse.mrt.ac.lk>
2017-11-06 06:38:27 +0530

    "Database pooling documented."

--- config/db.js
+++ config/db.js
@@ -5,6 +5,20 @@ var PRODUCTION_DB = 'schooldbms'
     , TEST_DB = 'schooldbms';
 
 /*
+Connection Pooling
+------------------
+
+* Establishing connections everytime a query is executed is an expensive operation.
+
+* Application Server enables administrators to establish a pool of backend connections
+that applications can share on an application server
+
+* This was initially a technique used in JDBC.
+
+* Since NodeJs is the major player in the web-industry these days, it was introduced inside the mysql driver
+in Node Packages.
+
+-Author: Dasun Pubudumal
 
  */
 
