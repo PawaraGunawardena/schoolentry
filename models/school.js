eb752052cadfa3328f1944ad7cba1b35189e2cef
PawaraGunawardena <pawara.15@cse.mrt.ac.lk>
2017-12-16 23:15:04 +0530

    school database connection

--- models/school.js
+++ models/school.js
@@ -3,3 +3,51 @@ var bcrypt = require('bcryptjs');
 var mysqlJson = require('mysql-json');
 var async = require('async');
 
+/*exports.insert = function (applicant ,pool, done) {
+    pool.getConnection(function (err, connection) {
+        if(err) throw err;
+        var query = connection.query('INSERT INTO school SET ?, name = (TIMESTAMPDIFF(YEAR,?,CURDATE()))', [applicant, applicant.date_of_birth], function (error, results) {
+            if(error) throw error;
+        });
+        console.log('Insert Query: ' + query);
+        console.log('School Inserted!');
+        connection.release();
+    });
+};*/
+
+exports.insert = function(school_id, name, postal_number,road,city,Province,max_value_of_grade_one_entries,buddhism_precentage,christianity_precentage,islam_precentage,Hindu_percentage,religion_others_precentage, pool, done){
+    bcrypt.hash(password, 8, function (err, hash) {
+        var school = {school_id: school_id,name:name,postal_number:postal_number,road:road,city:city,
+            Province:Province,max_value_of_grade_one_entries:max_value_of_grade_one_entries ,buddhism_precentage:buddhism_precentage,
+            christianity_precentage:christianity_precentage,islam_precentage:islam_precentage,
+            Hindu_percentage:Hindu_percentage,religion_others_precentage:religion_others_precentage};
+        pool.getConnection(function (err, connection) {
+            if (err) throw err;
+            var query = connection.query('INSERT INTO school SET ?', school, function (error, results) {
+                if(error) throw error;
+            });
+            console.log('Insert query: ' + query.sql);
+            console.log('Users Inserted!');
+
+            // if(user_type == 'moe_officer'){
+            //     var moe_officer = {
+            //         officer_moe_id:
+            //         id, post: 'moe_officer',
+            //         first_name: first_name,
+            //         last_name: last_name,
+            //         date_of_birth: dob,
+            //         nationality: nationality,
+            //         religion: religion,
+            //         gender: gender
+            //     };
+            //     var query2 = connection.query('INSERT INTO officer_ministry_of_education SET ?', moe_officer, function (err, results) {
+            //        if(err) throw err;
+            //     });
+            // };
+
+            connection.release();
+        });
+    });
+};
+
+
--- routes/schools.js
+++ routes/schools.js
@@ -10,5 +10,30 @@ module.exports = function(app, express, pool, schoolmodel){
         res.render('schoolindex');
     });
 
+    router.post('/school_details', function (req, res, next) {
+        // var school = {
+        //     school_id: '0',
+        //     name: req.body.school_name,
+        //     postal_number: req.body.school_posatal,
+        //     road: req.body.school_road,
+        //     // age: req.body.age,
+        //     city: req.body.school_city,
+        //     Province: req.body.province_dropdown,
+        //     max_value_of_grade_one_entries: req.body.school_max_students,
+        //     buddhism_percentage: app.locals.guardian.school_buddhism,
+        //     christianity_percentage: app.locals.guardian.school_christianity,
+        //     islam_percentage: app.locals.guardian.school_islamic,
+        //     Hindu_percentage: app.locals.guardian.school_hindu,
+        //     religion_others_percentage: app.locals.guardian.guardian_nic_no
+        // };
+
+
+
+        console.log(school.name);
+        schoolmodel.insert(school,pool);
+        // res.render('applicant-details',{username: req.user.username});
+        res.redirect('/school_details')
+    });
+
     return router;
 }
