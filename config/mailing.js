e6830e240faf853969a9e1d9264d02e2b555ee5a
Dasun Pubudumal <dasun.15@cse.mrt.ac.lk>
2017-12-17 14:25:19 +0530

    asdasd

--- config/mailing.js
+++ config/mailing.js
@@ -1,24 +1,2 @@
-'use strict';
-const nodemailer = require('nodemailer');
-
-const transporter = nodemailer.createTransport({
-    service: 'gmail',
-    auth: {
-           user: 'moeofficeclerk@gmail.com',
-           pass: 'moeofficeclerk'
-    }
-});
-
-const mailOptions = {
-    from: 'moeofficeclerk@gmail.com', // sender address
-    to: 'dasun.15@uomcse.lk', // list of receivers
-    subject: 'Accepted lists', // Subject line
-    html: '<p>Your html here</p>'// plain text body
-};
-
-exports.mail = transporter.sendMail(mailOptions, function(err, info){
-    if(err) throw err;
-    console.log(info);
-});
 
   
